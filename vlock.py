0284650820852d6aed59d20438939a02eec8920a
Nikos <Nikos@Colonel.local>
2017-01-31 09:07:06 +0100

    script

---
+++ vlock.py
@@ -0,0 +1,119 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+bl_info = {
+    "name": "Vlock",
+    "author": "Nikos",
+    "version": (0,1),
+    "blender": (2, 7, 8, 0),
+    "api": 44539,
+    "category": "3D View",
+    "location": "View3D > Header",
+    "description": "This addon helps you lock some vertices in place.",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",}
+''' version 0.1 '''
+
+import bpy
+from bpy.types import Header
+import bmesh
+
+global lock_ON
+global vertices
+
+lock_ON = False
+
+
+def VL_update(context):
+    """
+    """
+    global lock_ON
+    global vertices
+    edit_obj = bpy.context.edit_object
+    if edit_obj is not None and edit_obj.is_updated_data is True and lock_ON:
+        print('update needed')
+        mesh = bpy.context.scene.objects.active.data
+        bm = bmesh.from_edit_mesh(mesh)
+        for v in vertices:
+            bm.verts[v[0]].co.x = v[1]
+            bm.verts[v[0]].co.y = v[2]
+            bm.verts[v[0]].co.z = v[3]
+        bpy.context.scene.objects.active = bpy.context.scene.objects.active
+        bmesh.update_edit_mesh(mesh, True)
+    return True
+
+
+def lock():
+    global vertices
+    vertices = []
+    mesh = bpy.context.scene.objects.active.data
+    bm = bmesh.from_edit_mesh(mesh)
+    sel_verts = [ vert for vert in bm.verts if vert.select ]
+    for v in sel_verts:
+        row = [v.index, v.co.x,v.co.y,v.co.z]
+        vertices.append(row)
+
+
+class lock_OP(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "scene.lock_action"
+    bl_label = "Lock vertices"
+    
+    def execute(self, context):
+        global lock_ON
+        lock_ON = not lock_ON
+        if lock_ON:
+            lock()
+        print(lock_ON)
+        return {'FINISHED'}
+
+
+class vlock_button(Header):
+    bl_space_type = 'VIEW_3D'
+    bl_label = "Vlock"
+    bl_idname = "OBJECT_Vlock"
+    bl_region_type = 'HEADER'
+    bl_context = "scene"
+
+    def draw(self, context):
+        global lock_ON
+        edit_obj = bpy.context.edit_object
+        if edit_obj is not None :
+            layout = self.layout
+            row = layout.row()
+            if lock_ON:
+                row.operator("scene.lock_action", icon='PINNED', text='')
+            else:
+                row.operator("scene.lock_action", icon='UNPINNED', text='')
+            
+
+  
+def register():
+    bpy.utils.register_module(__name__)
+    bpy.app.handlers.scene_update_post.clear()
+    bpy.app.handlers.scene_update_post.append(VL_update)
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+    bpy.app.handlers.scene_update_post.remove(VL_update)
+
+
+if __name__ == "__main__":
+    register()
